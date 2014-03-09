2c8c743507e387cdc85e40e575d95c7523cb9d13
Ndeye Seynabou DIOP <ndeye-seynabou.diop@telecom-paristech.fr>
2014-03-09 23:39:19 +0100

    Creation d'une nouvelle methode d'envoi des positions

--- src/moduleDESCRIPTEUR/DescDEMO.java
+++ src/moduleDESCRIPTEUR/DescDEMO.java
@@ -1,5 +1,6 @@
 package moduleDESCRIPTEUR;
 import java.util.Random;
+
 import moduleClassif.*;
 
 
@@ -7,18 +8,18 @@ public class DescDEMO{
 	public DescDEMO(){
 		super();
 	}
-	
-	public void descripteur(){
-		
-//		Scanner sc = new Scanner(System.in);
-//
-//		System.out.println("Veuillez sélectionner le nombre d'électrodes :");
-//		int n = sc.nextInt();
+
+	public void descripteur() throws InterruptedException{
+
+		//		Scanner sc = new Scanner(System.in);
+		//
+		//		System.out.println("Veuillez sélectionner le nombre d'électrodes :");
+		//		int n = sc.nextInt();
 		int[] feature = new int[10];
 
 		Random r = new Random();
-
-		for (int i = 1; i <= 10; i++) {
+		while(true){
+			for (int i = 1; i <= 10; i++) {
 				float x;
 				x = r.nextFloat();
 
@@ -27,10 +28,11 @@ public class DescDEMO{
 				} else {
 					feature[i - 1] = 1;
 				}
-//				System.out.println(descripteur[i-1]);	
+				//				System.out.println(descripteur[i-1]);	
+			}
+			ClassifDEMO classifieur = new ClassifDEMO();
+			classifieur.giveMatrix(feature);
+			Thread.sleep(60);
 		}
-		ClassifDEMO classifieur = new ClassifDEMO();
-		classifieur.giveMatrix(feature);
 	}
-
 }
