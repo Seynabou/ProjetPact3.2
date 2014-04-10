5397bc0aea30ce54a287dd012b5fb103986f6920
zaccharieramzi <zaccharie.ramzi@gmail.com>
2014-04-10 12:21:36 +0200

    Déclarations en attributs des différents panneaux dont on aura besoin

--- src/moduleIG/FenetreConfig.java
+++ src/moduleIG/FenetreConfig.java
@@ -1,5 +1,18 @@
 package moduleIG;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class FenetreConfig {
 
+	private JFrame fenetre = new JFrame("Configurations");
+	private JPanel panneauPrinc = (JPanel) this.fenetre.getContentPane();
+	private JPanel panneauDecor = new JPanel();
+	private JPanel panneauDecorImage = new JPanel();
+	private JPanel panneauRaquette = new JPanel();
+	private JPanel panneauRaquetteImage = new JPanel();
+	private JPanel panneauDifficulte = new JPanel();
+	private JPanel panneauValidation = new JPanel();
+	
+	
 }
