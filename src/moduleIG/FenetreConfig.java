04a122cf8daa64a0b6a0e62fea784dee0d8ba2cb
zaccharieramzi <zaccharie.ramzi@gmail.com>
2014-04-10 12:24:39 +0200

    Suppression des dits panneaux : en fait je pense qu'il vaut mieux créer des classes à part vu la richesse du code

--- src/moduleIG/FenetreConfig.java
+++ src/moduleIG/FenetreConfig.java
@@ -7,12 +7,6 @@ public class FenetreConfig {
 
 	private JFrame fenetre = new JFrame("Configurations");
 	private JPanel panneauPrinc = (JPanel) this.fenetre.getContentPane();
-	private JPanel panneauDecor = new JPanel();
-	private JPanel panneauDecorImage = new JPanel();
-	private JPanel panneauRaquette = new JPanel();
-	private JPanel panneauRaquetteImage = new JPanel();
-	private JPanel panneauDifficulte = new JPanel();
-	private JPanel panneauValidation = new JPanel();
-	
+	private ConfigData donnes = new ConfigData();
 	
 }
