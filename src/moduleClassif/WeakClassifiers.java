1fe719797a8e37080a3bdf1abee0b864cb5c473a
Ndeye Seynabou DIOP <ndeye-seynabou.diop@telecom-paristech.fr>
2014-04-28 16:22:06 +0200

    WeakClassifiers OKAY

--- src/moduleClassif/WeakClassifiers.java
+++ src/moduleClassif/WeakClassifiers.java
@@ -2,23 +2,26 @@ package moduleClassif;
 
 public class WeakClassifiers {
 
-	int rows =4;  
+	int rows =5;  
 	int columns = 20;
 	double[][] MatrixOfFeatures = new double[rows][columns]; 
 	int[] labels = new int[rows];
 	int[] newLabels = new int[rows]; 
 	double[] weight = new double[rows];
-	
+
 	double[] errorFeatures = new double[columns];
 	double[] errorThreshold = new double[rows]; 
+	double[] tabValeur1= new double[rows];
+	double[] tabValeur2= new double[columns];
 	double error1, error2, error;
-	
+	int indiceThreshold;
+
 	int[] labelShape1= new int[rows];
 	int[] labelShape2= new int[columns];
 	int labelShape;
-	
+
 	int featureSelect;
-	double[] modelh = new double[3]; 
+	double[] modelh = new double[4]; 
 	double errorCalculated;
 
 	public WeakClassifiers(double[][] matrixOfFeatures, int[] labels, double[] weight){
@@ -28,7 +31,7 @@ public class WeakClassifiers {
 	}
 
 
-	public void Classifiers() { //changer le retour après pour predict: modelh[i]
+	public double[] Classifiers() { //changer le retour après pour adaboost: modelh[i]
 
 		//for each feature
 		//		for(int k =0; k<MatrixOfFeatures.length;k++){
@@ -36,109 +39,58 @@ public class WeakClassifiers {
 		//				System.out.println(k+" "+j+" ="+MatrixOfFeatures[k][j] + "/"+MatrixOfFeatures.length); 
 		//			}
 		//		}
-		
+
 		for(int p = 0; p<MatrixOfFeatures[0].length;p++){ 
 			for(int i =0; i<MatrixOfFeatures.length; i++){
 
 				//We assume in first time that to the right of threshold
 				//it is +1 and to his left, -1
 
-				//if(i==0){
-				for(int j=0 ; j<MatrixOfFeatures.length;j++){ 
-
-					if(MatrixOfFeatures[i][p] > MatrixOfFeatures[j][p]){
-						newLabels[j] = -1;
-					}
-
-					if(MatrixOfFeatures[i][p] <= MatrixOfFeatures[j][p]){
-						newLabels[j] = 1;
-					}
-					
-				}
+				newLabels=boubleForPourError1(MatrixOfFeatures, p, i);
 				error1 = error(labels,newLabels,weight);
-				//}
-				//				else{
-				//					for(int j=0 ; j<MatrixOfFeatures.length;j++){ 
-				//
-				//						if(MatrixOfFeatures[i][p] > MatrixOfFeatures[j][p]){
-				//							newLabels[j] = -1;
-				//						}
-				//
-				//						if(MatrixOfFeatures[i][p] <= MatrixOfFeatures[j][p]){
-				//							newLabels[j] = 1;
-				//						}
-				//						error1 = error(labels,newLabels,weight);
-				//					}	
-				//				}
-
 
 				//In second time, +1 at left and -1 at right
 
-
-				//if(i==0){
-				for(int j=0 ; j<MatrixOfFeatures.length;j++){ 
-
-					if(MatrixOfFeatures[i][p] > MatrixOfFeatures[j][p]){
-						newLabels[j] = 1;
-					}
-
-					if(MatrixOfFeatures[i][p] <= MatrixOfFeatures[j][p]){
-						newLabels[j] = -1;
-					}
-					
-				}
+				newLabels=boubleForPourError2(MatrixOfFeatures, p, i);
 				error2 = error(labels,newLabels,weight);
-				//				}
-				//				else{
-				//					for(int j=0 ; j<MatrixOfFeatures.length;j++){ 
-				//
-				//						if(MatrixOfFeatures[i][p] > MatrixOfFeatures[j][p]){
-				//							newLabels[j] = 1;
-				//						}
-				//
-				//						if(MatrixOfFeatures[i][p] <= MatrixOfFeatures[j][p]){
-				//							newLabels[j] = -1;
-				//						}
-				//						error2 = error(labels,newLabels,weight);
-				//					}	
-				//				}
 
 				if(error1 < error2){
-					errorThreshold[i] = error1;
-					labelShape1[i] = +1;				
+					this.errorThreshold[i] = error1;
+					//this.tabValeur1[i]=MatrixOfFeatures[i][p];
+					this.labelShape1[i] = +1;				
 				}
 				else{
-					errorThreshold[i] = error2;
-					labelShape1[i] = -1;
+					this.errorThreshold[i] = error2;
+					//this.tabValeur1[i]=MatrixOfFeatures[i][p];
+					this.labelShape1[i] = -1;
 				}
 			}
 
 			// determinate the best threshold of the feature
 			//which minimize the weighted error rate
-			double valeur=errorThreshold[0];
-			for(int j =1; j<errorThreshold.length;j++){	
-				if(valeur > errorThreshold[j]){ //attention
-					valeur = errorThreshold[j];
-					labelShape2[p] = labelShape1[j];
-				}
-			}
-			errorFeatures[p]=valeur;
+
+			//select the best errorThreshold
+			errorFeatures[p]=boucleForPourErrorThreshold(p);
+			tabValeur2[p]=MatrixOfFeatures[indiceThreshold][p];
 		}
-		double valeur2=errorFeatures[0];
-		for(int j =1; j<errorFeatures.length;j++){	
-			if(valeur2 > errorFeatures[j]){ 
-				valeur2 = errorFeatures[j];
-				labelShape = labelShape2[j];
-				featureSelect = j;
-			}
-		}
-		error = valeur2;
+		//select the best errorFeatures
+		error = boucleForPourErrorFeatures();
 
 		modelh[0] = featureSelect;
 		modelh[1] = error;
 		modelh[2] = labelShape;
+		modelh[3] = indiceThreshold; //correspond à la ligne sur la matrice sans ajout
 
-		System.out.println("feature "+modelh[0]+" error "+modelh[1]+" labelshape "+modelh[2]);
+		System.out.println("feature select "+modelh[0]+" error "+modelh[1]+" labelshape "+modelh[2]+" position ligne error "+modelh[3]+" " + tabValeur2[indiceThreshold]);
+
+//				for(int i=0; i<tabValeur2.length;i++){
+//					System.out.println(tabValeur2[i]);
+//				}
+//				for(int i=0; i<errorFeatures.length;i++){
+//					System.out.println(errorFeatures[i]);
+//				}
+				
+		return modelh;
 	}
 
 
@@ -154,6 +106,64 @@ public class WeakClassifiers {
 		}
 
 		return errorCalculated;		
+	}
+
+	public int[] boubleForPourError1(double[][]MatrixOfFeatures, int p, int i){
+		for(int j=0 ; j<MatrixOfFeatures.length;j++){ 
+
+			if(MatrixOfFeatures[i][p] > MatrixOfFeatures[j][p]){
+				newLabels[j] = -1;
+			}
+
+			if(MatrixOfFeatures[i][p] <= MatrixOfFeatures[j][p]){
+				newLabels[j] = 1;
+			}
+
+		}
+		return newLabels;
+	}
+
+	public int[] boubleForPourError2(double[][]MatrixOfFeatures, int p, int i){
+
+		for(int j=0 ; j<MatrixOfFeatures.length;j++){ 
+
+			if(MatrixOfFeatures[i][p] > MatrixOfFeatures[j][p]){
+				newLabels[j] = 1;
+			}
+
+			if(MatrixOfFeatures[i][p] <= MatrixOfFeatures[j][p]){
+				newLabels[j] = -1;
+			}
+
+		}
+		return newLabels;
+	}
+
+	public double boucleForPourErrorThreshold(int p){
+
+		double valeur=errorThreshold[0];
+
+		for(int j =1; j<errorThreshold.length;j++){	
+			if(valeur > errorThreshold[j]){ //attention
+				valeur = errorThreshold[j];
+				labelShape2[p] = labelShape1[j];
+				this.indiceThreshold = j; // donne le numero de la ligne de l'element choisi, va servir pour la comparaison after
+			}
+
+		}
+		return valeur;
+	}
+
+	public double boucleForPourErrorFeatures(){
+		double valeur2=errorFeatures[0];
+		for(int j =1; j<errorFeatures.length;j++){	//j va jusqu'à 20 
+			if(valeur2 > errorFeatures[j]){ 
+				valeur2 = errorFeatures[j];
+				labelShape = labelShape2[j];
+				featureSelect = j; // attention les tableaux en java commence par 0 donc featureSelect+1 pour matrice
+			}
+		}
+		return valeur2;
 	}
 
 }
