1124e7c4ad247f360ca4d404c9a8d93b8e45d045
eymericgenty <eymericgenty@aol.com>
2014-05-02 01:18:06 +0200

    Modif Exception out of bound

--- src/moduleDescripteur/Mediane.java
+++ src/moduleDescripteur/Mediane.java
@@ -1,13 +1,14 @@
 package moduleDescripteur;
+
 import Jama.Matrix;
 import java.util.Arrays;
 
 public class Mediane {
-	
+
 	Matrix M;
-	
-	public Mediane(Matrix M){
-		this.M=M;
+
+	public Mediane(Matrix M) {
+		this.M = M;
 	}
 
 	public Matrix GetMediane() {
@@ -26,75 +27,57 @@ public class Mediane {
 			Arrays.sort(tab[i]);
 		}
 
-		
-		 for (int i=0 ; i<N ; i++){ for (int j=0 ; j<T ; j++){
-		  System.out.println(tab[i][j]); } }
-		 
-		
-		//On détermine la médiane de chaque ligne
-		
+		// for (int i=0 ; i<N ; i++){ for (int j=0 ; j<T ; j++){
+		// System.out.println(tab[i][j]); } }
+
+		// On détermine la médiane de chaque ligne
+
 		for (int i = 0; i < N; i++) {
 			if (T % 2 == 0) {
 				Middle[i] = tab[i][T / 2];
 			} else {
 				Middle[i] = (tab[i][(T - 1) / 2] + tab[i][(T + 1) / 2]) / 2;
+			}
+		}
 
-			}
-		}
-		
-		Matrix Ma = new Matrix(Middle,1);
-		
-		
-		
-		
-		
-		/*
-		//On retire cette médiane à chaque élément de chaque ligne
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < T; j++) {
-				M.set(i, j, M.get(i, j)-Middle[i]);
-			}
-		
-		}
-		*/
-		
-		
-		
-		
-		
-		/*
-		
-		// Calcul de la médiane de la matrice
-		
-		double[] tab2 = new double[N*T];
-		int t=0;
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < T; j++) {
-				tab2[t] = M.get(i, j);
-				t=t+1;
-			}
-			t=t+1;
-		}
-		Arrays.sort(tab2);
-		
-		double Middle2;
-			if ((N*T) % 2 == 0) {
-				Middle2 = tab2[(N*T) / 2];
-			} else {
-				Middle2 = (tab2[((N*T) - 1) / 2] + tab2[((N*T) + 1) / 2]) / 2;
+		double[][] A = new double[Middle.length][1];
 
-			}
-			
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < T; j++) {
-					M.set(i, j, M.get(i, j)-Middle2);
-				}
-			
-			}
-		
-		*/
-		
+		for (int i = 0; i < Middle.length; i++) {
+			A[i][0] = Middle[i];
+		}
+
+		Matrix Ma = new Matrix(A);
+
+	
+
+		/*
+		 * //On retire cette médiane à chaque élément de chaque ligne for (int i
+		 * = 0; i < N; i++) { for (int j = 0; j < T; j++) { M.set(i, j, M.get(i,
+		 * j)-Middle[i]); }
+		 * 
+		 * }
+		 */
+
+		/*
+		 * 
+		 * // Calcul de la médiane de la matrice
+		 * 
+		 * double[] tab2 = new double[N*T]; int t=0;
+		 * 
+		 * for (int i = 0; i < N; i++) { for (int j = 0; j < T; j++) { tab2[t] =
+		 * M.get(i, j); t=t+1; } t=t+1; } Arrays.sort(tab2);
+		 * 
+		 * double Middle2; if ((N*T) % 2 == 0) { Middle2 = tab2[(N*T) / 2]; }
+		 * else { Middle2 = (tab2[((N*T) - 1) / 2] + tab2[((N*T) + 1) / 2]) / 2;
+		 * 
+		 * }
+		 * 
+		 * for (int i = 0; i < N; i++) { for (int j = 0; j < T; j++) { M.set(i,
+		 * j, M.get(i, j)-Middle2); }
+		 * 
+		 * }
+		 */
+
 		return (Ma);
 	}
 }
--- src/moduleDescripteur/Projection.java
+++ src/moduleDescripteur/Projection.java
@@ -4,9 +4,10 @@ import Jama.Matrix;
 
 public class Projection {
 
+	private Matrix[] tableauMatrice;
 	private Matrix X;
 	private Matrix W;
-	int k= 2;				// a modifier
+	int k = 2; // a modifier
 
 	public Projection(Matrix X) {
 		this.X = X;
@@ -14,13 +15,15 @@ public class Projection {
 
 	// Attention à la dimension de X, il doit avoir un nombre pair de colonne
 
-	
+	public Projection(Matrix[] tableauMatrice) {
+		this.tableauMatrice = tableauMatrice;
+	}
+
 	public Matrix MatriceRedimensionnee() {
-		
+
 		MatriceProjection matriceProjection = new MatriceProjection();
 		W = matriceProjection.Apprentissage();
-		
-		
+
 		Matrix M = new Matrix(2 * k, W.getColumnDimension());
 
 		for (int j = 0; j < M.getColumnDimension(); j++) {
@@ -45,19 +48,39 @@ public class Projection {
 		Matrix Z;
 		Matrix Xd;
 
-	
 		Z = this.MatriceRedimensionnee().times(X);
 		Mediane Me = new Mediane(Z);
 		Xd = Me.GetMediane();
+		
+	//	for ( int i=0 ; i<Xd.getRowDimension() ; i++){
+		//	for (int j=0 ; j<Xd.getColumnDimension() ; j++){
+		//		System.out.println(Xd.get(i, j));
+		//	}
+	//	}
 
 		double[] VectorOfFeature = new double[Xd.getRowDimension()];
 
 		for (int i = 0; i < Xd.getRowDimension(); i++) {
-			VectorOfFeature[i] = Xd.get(i, 1);
+			VectorOfFeature[i] = Xd.get(i, 0); 
 		}
 
 		return (VectorOfFeature);
 
+	}
+
+	public double[][] TableauDescripteur() {
+
+		double[][] MatrixOfVectorOfFeatures = new double[tableauMatrice[0]
+				.getRowDimension()][tableauMatrice.length];
+
+		Projection[] descripteur = new Projection[k];
+
+		for (int i = 0; i < tableauMatrice.length; i++) {
+			descripteur[i] = new Projection(tableauMatrice[i]);
+			MatrixOfVectorOfFeatures[i] = descripteur[i].Descripteur();
+
+		}
+		return (MatrixOfVectorOfFeatures);
 	}
 
 }
