7c23d9f4f2be033961c8b6701ff160e41e5cd40a
Seynabou <zeynab192@gmail.com>
2014-05-01 20:36:32 +0200

    Delete TESTDescripteurClassif.java

--- src/moduleDESCRIPTEUR/TESTDescripteurClassif.java
+++
@@ -1,9 +0,0 @@
-package moduleDESCRIPTEUR;
-
-public class TESTDescripteurClassif{
-	public static void main(String[] args) throws InterruptedException{
-		DescDEMO descripteur = new DescDEMO();
-		descripteur.descripteur();
-	}
-}
-
